db53c77207bbd934b3fea157fffd9588b3a8ff6a
Guus der Kinderen <guus.der.kinderen@gmail.com>
2021-06-29 16:45:19 +0200

    OF-2272: Replace Local- and Remote- MUC entities with one generic implementation.
    An issue arises from having a concept of “local” and “remote” MUC users and roles. In this context, ‘local’ refers to a entity that is connected to the local cluster node, while “remote” refers to an entity connected to another node in the cluster. This distinction cannot be made for entities that join a MUC room from remote domains (through server-to-server functionality). Server-to-server connectivity can arbitrarily be established to any one, but also multiple, nodes of the cluster. Worse, a server-to-server connection can be teared down, and re-established to a new cluster node. Because of this, it’s not possible to uniquely qualify a MUC user that is a user from another XMPP domain as a ‘remote’ or ‘local’ MUC user. It can be either, or both, but also change over time.
    
    This commit broadly removes the concept of having “local” and “remote” entities, replacing them with a singular representation.
    
    More work is needed to properly maintain a singular collection of entities in a shared memory datastructure.

--- xmppserver/src/main/java/org/jivesoftware/openfire/muc/spi/IQOwnerHandler.java
+++ xmppserver/src/main/java/org/jivesoftware/openfire/muc/spi/IQOwnerHandler.java
@@ -55,7 +55,7 @@ public class IQOwnerHandler {
     
     private static final Logger Log = LoggerFactory.getLogger(IQOwnerHandler.class);
 
-    private final LocalMUCRoom room;
+    private final MUCRoom room;
 
     private final PacketRouter router;
 
@@ -65,7 +65,7 @@ public class IQOwnerHandler {
 
     private final boolean skipInvite;
 
-    public IQOwnerHandler(LocalMUCRoom chatroom, PacketRouter packetRouter) {
+    public IQOwnerHandler(MUCRoom chatroom, PacketRouter packetRouter) {
         this.room = chatroom;
         this.router = packetRouter;
         this.skipInvite = JiveGlobals.getBooleanProperty(
@@ -408,7 +408,7 @@ public class IQOwnerHandler {
         if (ownersSent) {
             // Change the affiliation to "member" for the current owners that won't be neither
             // owner nor admin (if the form included the owners field)
-            List<JID> ownersToRemove = new ArrayList<>(room.owners);
+            List<JID> ownersToRemove = new ArrayList<>(room.getOwners());
             ownersToRemove.removeAll(admins);
             ownersToRemove.removeAll(owners);
             for (JID jid : ownersToRemove) {
@@ -422,7 +422,7 @@ public class IQOwnerHandler {
         if (adminsSent) {
             // Change the affiliation to "member" for the current admins that won't be neither
             // owner nor admin (if the form included the admins field)
-            List<JID> adminsToRemove = new ArrayList<>(room.admins);
+            List<JID> adminsToRemove = new ArrayList<>(room.getAdmins());
             adminsToRemove.removeAll(admins);
             adminsToRemove.removeAll(owners);
             for (JID jid : adminsToRemove) {
@@ -446,8 +446,7 @@ public class IQOwnerHandler {
     }
 
     private void refreshConfigurationFormValues() {
-        room.lock.readLock().lock();
-        try {
+        synchronized (room) {
             FormField field = configurationForm.getField("muc#roomconfig_roomname");
             field.clearValues();
             field.addValue(room.getNaturalLanguageName());
@@ -563,9 +562,6 @@ public class IQOwnerHandler {
             // Add the new representation of configurationForm as an element 
             probeResult.add(configurationForm.getElement());
 
-        }
-        finally {
-            room.lock.readLock().unlock();
         }
     }
 
